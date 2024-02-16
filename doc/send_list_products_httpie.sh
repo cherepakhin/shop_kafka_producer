70d065cef386aae8b2c32c5f2f3b4cac07aa450e
Черепахин Василий <vasi.che@gmail.com>
2024-02-16 22:29:53 +0500

    add send_list_products_httpie.sh

--- doc/send_list_products_httpie.sh
+++ doc/send_list_products_httpie.sh
@@ -1,1 +1,11 @@
-http POST http://127.0.0.1:8990/shop_kafka_producer/api/send_product/echo/array_message < ./doc/list_10products.json
+max=1000
+json_file="list_10_products.json"
+echo -ne "[\n" > $json_file
+for i in $(seq 1 $max)
+do
+    echo "{\"n\": $i, \"name\": \"NAME_$i\", \"groupDtoN\":100}, " >> $json_file
+done
+echo "{\"n\":" $((max+1)), "\"name\": \"NAME_$((max+1))\", \"groupDtoN\":100}" >> $json_file
+echo "]" >> $json_file
+
+http POST http://127.0.0.1:8990/shop_kafka_producer/api/send_product/send_list < $json_file
