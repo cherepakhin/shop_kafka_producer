6f583579bd207aaa5f2ebfd87503ec9a8e8befac
Черепахин Василий <vasi.che@gmail.com>
2024-02-22 12:43:18 +0500

    add documentation in generate_list_for_send.sh

--- doc/generate_list_for_send.sh
+++ doc/generate_list_for_send.sh
@@ -1,11 +1,15 @@
 # Generate list for send to /echo/array_message and etc...
 
 max=100
-json_file="list_products.json"
-echo -ne "[\n" > $json_file
+json_file="test_list_products.txt"
+# uncomment the next line if you want to generate a new file with the array json
+#echo -ne "[\n" > $json_file
 for i in $(seq 1 $max)
 do
-    echo "{\"n\": $i, \"name\": \"NAME_$i\", \"groupDtoN\":100}, " >> $json_file
+    echo "{\"n\": $i, \"name\": \"NAME_$i\", \"groupDtoN\":100}" >> $json_file
+# uncomment the next line if you want to generate a new file with the array json
+#   echo -ne ",\n" >> $json_file
 done
-echo "{\"n\":" $((max+1)), "\"name\": \"NAME_$((max+1))\", \"groupDtoN\":100}" >> $json_file
-echo "]" >> $json_file
+# uncomment the next line if you want to generate a new file with the array json
+#echo "{\"n\":" $((max+1)), "\"name\": \"NAME_$((max+1))\", \"groupDtoN\":100}" >> $json_file
+#echo "]" >> $json_file
