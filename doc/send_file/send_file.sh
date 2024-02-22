e1f1d21e9e794e11b935940c6ed5431ce28a1e57
Черепахин Василий <vasi.che@gmail.com>
2024-02-22 12:34:38 +0500

    add script send_file.sh for manual SEND json file

--- doc/send_file/send_file.sh
+++ doc/send_file/send_file.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# This script is used to send a file to a remote kafka topic
+
+# cat list_products.txt:
+#{"n": 1, "name": "NAME_1", "groupDtoN":100}
+#{"n": 2, "name": "NAME_2", "groupDtoN":100}
+#{"n": 3, "name": "NAME_3", "groupDtoN":100}
+#{"n": 4, "name": "NAME_4", "groupDtoN":100}
+#{"n": 101, "name": "NAME_101", "groupDtoN":100}
+
 # run from directory: doc/send_file/
+
 cat ./list_products.txt | /opt/kafka/bin/kafka-console-producer.sh --broker-list 192.168.1.20:9092 --topic product_ext_dto_topic
